579faf27cef617f30278d4eeae9e4c5589b39f73
Olle Harju <olle.harju@mrgreen.com>
2018-08-15 09:31:46 +0200

    #53 - Update splash animation * Updated js refresh logic. * Add random animation names. * Add css animations swing and jello.

--- src/styleguide/kss-assets/js/styleguide-custom.js
+++ src/styleguide/kss-assets/js/styleguide-custom.js
@@ -1,14 +1,31 @@
 (function(window) {
+    var refreshTimer;
+
     boot();
 
     function boot() {
-        refreshBounch();
+        refreshAnimation();
     }
 
-    function refreshBounch() {
-        var logo = window.document.querySelector("[sg-hero-img]");
-        var animated = !logo.getAttribute("animated") ? logo.setAttribute("animated", "rubberBand") : logo.setAttribute("animated", "");
+    function refreshAnimation() {
+        var selector = "[sg-hero-img]";
+        var attrAnimationName = "animated";
+        var delay = 2000;
 
-        return window.setTimeout(refreshBounch, 2000);
+        refreshTimer = setTimeout(function refreshTimer() {
+            var logo = window.document.querySelector(selector);
+            console.log();
+            var animated = !logo.getAttribute(attrAnimationName)
+                ? logo.setAttribute(attrAnimationName, randomAnimation())
+                : logo.setAttribute(attrAnimationName, "");
+
+            timerId = setTimeout(refreshTimer, delay);
+        }, delay);
+    }
+
+    function randomAnimation() {
+        var animations = ["rubberBand", "swing", "jello"];
+
+        return animations[Math.floor(Math.random() * animations.length)];
     }
 })(window);
