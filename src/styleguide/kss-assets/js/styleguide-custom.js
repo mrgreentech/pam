d5a92a89d716a8208bbddd11876f5f98242b527e
Olle Harju <olle.harju@gmail.com>
2018-09-30 04:11:27 +0200

    #55 - Update skinSwitcher to contain all helper functions

--- src/styleguide/kss-assets/js/styleguide-custom.js
+++ src/styleguide/kss-assets/js/styleguide-custom.js
@@ -4,9 +4,6 @@
  * @return {undefined}
  */
 (function(window) {
-    var skinStylesheet;
-    var skinsData;
-    var skinsEnabled;
     var storage = {
         getItem: function(key) {
             return JSON.parse(localStorage.getItem(key));
@@ -16,211 +13,235 @@
         }
     };
 
-    // var skinSwitcher = (function(window) {})(window);
+    var skinSwitcher = (function(window, storage) {
+        // Constants
+        var CSS_SRC_PATH = "kss-assets/css/";
+        var PAM_FILENAME = "pam.css";
 
-    function bakeElement(tagName, appendTo, properties, action) {
-        var element = document.createElement(tagName);
+        // Module vars
+        var defaultLinkElement;
+        var defaultSkinName;
+        var selectParent;
+        var skinsData;
+        var skinsEnabled;
+        var skinsPlaceholder;
 
-        if (properties) {
-            for (var property in properties) {
-                element.setAttribute(property, properties[property]);
+        // Public
+        var pub = {
+            enable: enable,
+            setSelectListener: setSelectListener,
+            isSelectParent: isSelectParent,
+            listenOnSelectChange: listenOnSelectChange
+        };
+
+        return pub;
+
+        // Functions
+        function enable(placeholderSelector) {
+            skinsData = window.skinsData;
+            skinsEnabled = skinsData && skinsData.length >= 2;
+            skinsPlaceholder = document.querySelector(placeholderSelector);
+            defaultSkinName = getDefaultSkinName(skinsData);
+
+            if (skinsEnabled) {
+                defaultLinkElement = getDefaultLinkElement();
+                setAlternateStyleSheets(skinsData, defaultLinkElement);
+                setActiveStyleSheet(getTitle(skinsData));
+
+                if (skinsEnabled && skinsPlaceholder) {
+                    selectParent = createSelect(skinsData, skinsPlaceholder);
+                }
             }
         }
 
-        return action(element, appendTo);
-    }
+        function getDefaultSkinName(data) {
+            return data.filter(function(item) {
+                return item.path === PAM_FILENAME;
+            })[0].name;
+        }
 
-    function setAlternateStyleSheets(data, defaultStyleSheet) {
-        var sourcePath = "kss-assets/css/";
+        function getDefaultLinkElement() {
+            return document.querySelector("link[href='" + CSS_SRC_PATH + PAM_FILENAME + "'");
+        }
 
-        data.forEach(function(item, index) {
-            if (item.path !== "pam.css") {
-                bakeElement(
-                    "link",
-                    defaultStyleSheet.nodeEl,
-                    {
-                        rel: "alternate stylesheet",
-                        href: sourcePath + item.path,
-                        id: item.path.split(".")[0],
-                        title: item.name
-                    },
-                    function(element, appendTo) {
-                        appendTo.after(element);
+        function getPreferredStyleSheet() {
+            var i, a;
+            for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
+                if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("rel").indexOf("alt") == -1 && a.getAttribute("title")) {
+                    return a.getAttribute("title");
+                }
+            }
+            return null;
+        }
+
+        function setAlternateStyleSheets(data, defaultStyleSheet) {
+            defaultStyleSheet.setAttribute("title", "default skin");
+            data.forEach(function(item, index) {
+                if (item.path !== "pam.css") {
+                    bakeElement(
+                        "link",
+                        defaultStyleSheet,
+                        {
+                            rel: "alternate stylesheet",
+                            href: CSS_SRC_PATH + item.path,
+                            id: item.path.split(".")[0],
+                            title: item.name
+                        },
+                        function(element, appendTo) {
+                            appendTo.after(element);
+                        }
+                    );
+                }
+            });
+        }
+
+        function setActiveStyleSheet(title) {
+            var i;
+            var link;
+
+            for (i = 0; (link = document.getElementsByTagName("link")[i]); i++) {
+                var preferredAndAlternate = link.getAttribute("rel").indexOf("style") != -1 && link.getAttribute("title");
+                var setActive = link.getAttribute("title") == title;
+
+                if (preferredAndAlternate) {
+                    link.disabled = true;
+
+                    if (setActive) {
+                        link.disabled = false;
                     }
-                );
+                }
             }
-        });
-    }
+        }
 
-    function createSelect(data, appendTo) {
-        // Create form
-        var skinForm = bakeElement(
-            "form",
-            "",
-            {
-                id: "skin-form",
-                "pam-Form": "stacked fluid"
-            },
-            function(element, appendTo) {
-                element.style.height = "48px";
-                return element;
+        function isValidStyleSheet(data, wantedStyleSheet) {
+            return data.some(function(item) {
+                return item.name === wantedStyleSheet;
+            });
+        }
+
+        function isSelectParent() {
+            return selectParent !== undefined;
+        }
+
+        function getTitle(data) {
+            var storedTitle = storage.getItem("title");
+            return isValidStyleSheet(data, storedTitle) ? storedTitle : getPreferredStyleSheet();
+        }
+
+        function setTitle(data, title) {
+            var titleToStore = isValidStyleSheet(data, title) ? title : getPreferredStyleSheet();
+            return storage.setItem("title", titleToStore);
+        }
+
+        function setTitleAndActiveStyleSheet(value) {
+            setActiveStyleSheet(value);
+            storage.setItem("title", value);
+        }
+
+        function bakeElement(tagName, appendTo, properties, action) {
+            var element = document.createElement(tagName);
+
+            if (properties) {
+                for (var property in properties) {
+                    element.setAttribute(property, properties[property]);
+                }
             }
-        );
 
-        // Create skin select
-        var skinSelect = bakeElement(
-            "select",
-            skinForm,
-            {
-                id: "skin-select",
-                "pam-Select": "",
-                animated: "fadeIn"
-            },
-            function(element, appendTo) {
-                data.forEach(function(item, index) {
-                    var preferredSkin = item.name === storage.getItem("title");
+            return action(element, appendTo);
+        }
 
-                    //TODO: Add case for default option when there is no latest.
-                    if (preferredSkin) {
-                        element.options[element.options.length] = new Option(item.name, item.name, true, true);
-                    } else if (item.name === "default skin") {
-                        element.options[element.options.length] = new Option(item.name, item.name, true, true);
-                    } else {
-                        element.options[element.options.length] = new Option(item.name, item.name);
-                    }
-                });
-                return appendTo.appendChild(element);
+        function listenOnSelectChange() {
+            setSelectListener("#skin-select", function(evt) {
+                var src = evt.target || evt.srcElement;
+
+                setTitleAndActiveStyleSheet(src.value);
+            });
+        }
+
+        function setSelectListener(selector, action) {
+            if (!selector) {
+                throw new Error("A selector is required.");
             }
-        );
+            if (!(typeof action === "function")) {
+                throw new Error("Action needs to be a function.");
+            }
 
-        window.requestAnimationFrame(function() {
-            appendTo.appendChild(skinForm);
-        });
-    }
+            var select = document.querySelector(selector);
 
-    function isValidStyleSheet(data, wantedStyleSheet) {
-        return data.some(function(item) {
-            return item.name === wantedStyleSheet;
-        });
-    }
+            if (select) {
+                select.onchange = function(evt) {
+                    action(evt);
+                };
+            }
 
-    function getTitle(data) {
-        var storedTitle = storage.getItem("title");
-        return isValidStyleSheet(data, storedTitle) ? storedTitle : getPreferredStyleSheet();
-    }
+            return select;
+        }
 
-    function setTitle(data, title) {
-        var titleToStore = isValidStyleSheet(data, title) ? title : getPreferredStyleSheet();
-        return storage.setItem("title", titleToStore);
-    }
+        function createSelect(data, appendTo) {
+            // Create form
+            var skinForm = bakeElement(
+                "form",
+                "",
+                {
+                    id: "skin-form",
+                    "pam-Form": "stacked fluid"
+                },
+                function(element, appendTo) {
+                    element.style.height = "48px";
+                    return element;
+                }
+            );
 
-    function setTitleAndActiveStyleSheet(value) {
-        setActiveStyleSheet(value);
-        storage.setItem("title", value);
-    }
+            // Create skin select
+            bakeElement(
+                "select",
+                skinForm,
+                {
+                    id: "skin-select",
+                    "pam-Select": "",
+                    animated: "fadeIn"
+                },
+                function(element, appendTo) {
+                    data.forEach(function(item, index) {
+                        var preferredSkin = item.name === storage.getItem("title");
+
+                        //TODO: Add case for default option when there is no latest.
+                        if (preferredSkin) {
+                            element.options[element.options.length] = new Option(item.name, item.name, true, true);
+                        } else if (item.name === "default skin") {
+                            element.options[element.options.length] = new Option(item.name, item.name, true, true);
+                        } else {
+                            element.options[element.options.length] = new Option(item.name, item.name);
+                        }
+                    });
+                    return appendTo.appendChild(element);
+                }
+            );
+
+            return window.requestAnimationFrame(function() {
+                return appendTo.appendChild(skinForm);
+            });
+        }
+    })(window, storage);
 
     document.addEventListener("DOMContentLoaded", function(evt) {
-        skinsData = window.skinsData;
-        skinsEnabled = skinsData && skinsData.length >= 2;
-
-        var skinsPlaceholder = document.querySelector("main aside header");
-        var skinsSelectEnabled = skinsEnabled && skinsPlaceholder;
-
-        if (skinsEnabled) {
-            skinStylesheet = getSkinStylesheet();
-            skinStylesheet.nodeEl.setAttribute("title", "default skin");
-            setAlternateStyleSheets(skinsData, skinStylesheet);
-            setActiveStyleSheet(getTitle(skinsData));
-
-            if (skinsSelectEnabled) {
-                createSelect(skinsData, skinsPlaceholder);
-            }
-        }
+        skinSwitcher.enable("main aside header");
     });
 
     window.onload = init;
 
     function init() {
-        if (skinsEnabled) {
-            setSelectListener("#skin-select", function(evt) {
-                var src = evt.target || evt.srcElement;
-                setTitleAndActiveStyleSheet(src.value);
-                // setActiveStyleSheet(src.value);
-                // storage.setItem("title", src.value);
-            });
+        if (skinSwitcher.isSelectParent()) {
+            skinSwitcher.listenOnSelectChange();
         }
 
         refreshAnimation("[sg-hero-img]");
-    }
-
-    function setActiveStyleSheet(title) {
-        var i;
-        var link;
-
-        for (i = 0; (link = document.getElementsByTagName("link")[i]); i++) {
-            var preferredAndAlternate = link.getAttribute("rel").indexOf("style") != -1 && link.getAttribute("title");
-            var setActive = link.getAttribute("title") == title;
-
-            if (preferredAndAlternate) {
-                link.disabled = true;
-
-                if (setActive) {
-                    link.disabled = false;
-                }
-            }
-        }
-    }
-
-    function getPreferredStyleSheet() {
-        var i, a;
-        for (i = 0; (a = document.getElementsByTagName("link")[i]); i++) {
-            if (a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("rel").indexOf("alt") == -1 && a.getAttribute("title")) {
-                return a.getAttribute("title");
-            }
-        }
-        return null;
-    }
-
-    function setSelectListener(selector, action) {
-        if (!selector) {
-            throw new Error("A selector is required.");
-        }
-        if (!(typeof action === "function")) {
-            throw new Error("Action needs to be a function.");
-        }
-
-        var select = document.querySelector(selector);
-
-        if (select) {
-            select.onchange = function(evt) {
-                action(evt);
-            };
-        }
-
-        return select;
     }
 
     function getRandomAnimation() {
         var animations = ["rubberBand", "swing", "jello"];
 
         return animations[Math.floor(Math.random() * animations.length)];
-    }
-
-    function getSkinStylesheet() {
-        var skinCssPath = "kss-assets/css/";
-        var linkStylesheets = document.querySelectorAll('link[rel="stylesheet"');
-        var skinLinkStylesheet = undefined;
-
-        for (var i = linkStylesheets.length - 1; i >= 0; i--) {
-            if (linkStylesheets[i].getAttribute("href").indexOf("pam.css") != -1) {
-                skinLinkStylesheet = linkStylesheets[i];
-            }
-        }
-
-        return {
-            nodeEl: skinLinkStylesheet,
-            cssPath: skinCssPath
-        };
     }
 
     function refreshAnimation(selector) {
