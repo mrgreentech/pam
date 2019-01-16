ca073dd8e744266eb2af669335aec2da69e38a71
Olle Harju <olle.harju@mrgreen.com>
2019-01-16 16:06:24 +0100

    feat(gulp): #67 update build tasks
    * Add plumber
    * Add eslint
    * Add stylelint
    * Add kss
    * remove run-sequence
    * update npm build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 // Config
 const config = require("./build.conf.js")();
+const kssConfig = require("./kss-config.json");
 
 // Modules
 const babel = require("gulp-babel");
@@ -9,15 +10,18 @@ const banner = require("gulp-banner");
 const cleanCss = require("gulp-clean-css");
 const concat = require("gulp-concat");
 const del = require("del");
+const eslint = require("gulp-eslint");
 const gulp = require("gulp");
 const gzip = require("gulp-gzip");
 const less = require("gulp-less");
 const lessPluginAutoprefix = require("less-plugin-autoprefix");
+const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const replace = require("gulp-replace");
 const sizeReporter = require("gulp-sizereport");
+const stylelint = require("gulp-stylelint");
 
-// const plugins = require("gulp-load-plugins")(config.plugins);
+const kss = require("kss");
 
 // // Cleaning
 // gulp.task("clean-build", () => {
@@ -170,6 +174,7 @@ function copyPamToSG() {
 function replaceVersion() {
     return gulp
         .src(config.build.styleguideIndexFile)
+        .pipe(plumber())
         .pipe(replace("[[version]]", config.version))
         .pipe(gulp.dest(config.build.styleguide));
 }
@@ -177,6 +182,7 @@ function replaceVersion() {
 function concatBase() {
     return gulp
         .src([config.node.normalize, config.build.lessFileBase])
+        .pipe(plumber())
         .pipe(concat("base.less"))
         .pipe(
             banner(config.banner, {
@@ -189,13 +195,17 @@ function concatBase() {
 function concatFont() {
     return gulp
         .src([config.build.lessFileFont, config.build.lessFileBase])
+        .pipe(plumber())
         .pipe(concat("base.less"))
         .pipe(gulp.dest(config.build.less));
 }
 
-function styles() {
+// Styles
+
+function css() {
     return gulp
         .src([config.build.lessFile, config.skin.lessFileGlob])
+        .pipe(plumber())
         .pipe(
             less({
                 plugins: [new lessPluginAutoprefix()]
@@ -204,17 +214,23 @@ function styles() {
         .pipe(gulp.dest(config.build.base));
 }
 
-function transpileJS() {
+function cssLint() {
     return gulp
-        .src(["src/js/*.js"])
-        .pipe(babel())
-        .pipe(gulp.dest("build/styleguide/kss-assets/js/"));
+        .src("src/less/**/*.less")
+        .pipe(plumber())
+        .pipe(
+            stylelint({
+                failAfterError: true,
+                reporters: [{ formatter: "string", console: true }]
+            })
+        );
 }
 
 // Optimize
-function minify() {
+function cssMinify() {
     return gulp
         .src(config.build.cssFile)
+        .pipe(plumber())
         .pipe(
             cleanCss({
                 compatibility: "*",
@@ -230,31 +246,65 @@ function minify() {
         .pipe(gulp.dest(config.build.base));
 }
 
+// Scripts
+
+function js() {
+    return gulp
+        .src(["src/js/*.js"])
+        .pipe(plumber())
+        .pipe(babel())
+        .pipe(gulp.dest("build/styleguide/kss-assets/js/"));
+}
+
+function jsLint() {
+    return gulp
+        .src(["src/**/*.js", "gulpfile.js"])
+        .pipe(plumber())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+}
+
 function compress() {
     return gulp
         .src(config.build.base + "/pam.min.css")
+        .pipe(plumber())
         .pipe(gzip())
         .pipe(gulp.dest(config.build.base));
 }
 
 function sizeReport() {
-    return gulp.src("./build/*").pipe(
-        sizeReporter({
-            gzip: true,
-            "*": {
-                maxSize: 20000
-            }
-        })
-    );
+    return gulp
+        .src("./build/*")
+        .pipe(plumber())
+        .pipe(
+            sizeReporter({
+                gzip: true,
+                "*": {
+                    maxSize: 20000
+                }
+            })
+        );
+}
+
+function styleguide() {
+    return kss(kssConfig);
 }
 
 // Complex tasks
+const buildStyleGuide = gulp.series(copyPamToSG, styleguide, replaceVersion);
+const styles = gulp.series(cssLint, css, cssMinify);
+const scripts = gulp.series(jsLint, js);
+const stylesAndScripts = gulp.parallel(styles, scripts);
 const concatFiles = gulp.series(concatBase, concatFont);
-const build = gulp.series(cleanBuild, copyBuild, concatFiles, styles, transpileJS, minify, copyPamToSG);
-const buildDev = gulp.series(copyBuild, concatFiles, styles, transpileJS, minify, copyPamToSG);
+const build = gulp.series(cleanBuild, copyBuild, concatFiles, stylesAndScripts, buildStyleGuide, sizeReport);
+const buildDev = gulp.series(copyBuild, concatFiles, stylesAndScripts, buildStyleGuide);
 const dist = gulp.series(cleanDist, copyDist);
 
 // export tasks
+exports.styleguide = styleguide;
+exports.cssLint = cssLint;
+exports.jsLint = jsLint;
 exports.sizeReport = sizeReport;
 exports.compress = compress;
 exports.replaceVersion = replaceVersion;
