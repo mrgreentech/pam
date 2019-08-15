552d1d3e1c727c98d7f046c097d93176fcf24c66
Olle Harju <olle.harju@mrggroup.com>
2019-08-15 15:11:22 +0200

    feat: #74 add base to variables doc script

--- scripts/lessToJS.js
+++ scripts/lessToJS.js
@@ -1,10 +1,52 @@
 const lessToJs = require("less-vars-to-js");
 const fs = require("fs");
 
-// Read the less file in as string
-const paletteLess = fs.readFileSync("src/less/variables.less", "utf8");
+const table = `
+Variables
 
-// Pass in file contents
-const palette = lessToJs(paletteLess, {});
+<table pam-Table="horizontal fluid">
+    <thead>
+        <tr>
+            <th>Folder</th>
+            <th>Description</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><code>/src</code></td>
+            <td>Contains Less, style guide and static sources such as images.</td>
+        </tr>
+        <tr>
+            <td><code>/dist</code></td>
+            <td>Contains Less, compiled CSS and style guide of current release.</td>
+        </tr>
+    </tbody>
+</table>
 
-console.log(palette);
+ Weight: 20
+
+ Style guide: variables
+`;
+
+// const variableLess = fs.readFileSync("build/less/variables.less", "utf8");
+
+fs.readFile("build/less/variables.less", "utf8", function(err, data) {
+    console.log(data);
+    const variables = lessToJs(data, {});
+    const variablesReplaced = data.replace(/<VARIABLES>/gi, table);
+    console.log(variables);
+    console.log(variablesReplaced);
+    return variablesReplaced;
+    // fs.writeFileSync("build/less/varia'bles.less", variablesReplaced, "utf8");
+});
+
+// Get less variables
+// const variables = lessToJs(variableLess, {});
+
+// Insert varibles in  file
+// const variablesReplaced = variableLess.replace(/<VARIABLES>/gi, table);
+
+// console.log(variables);
+// console.log(variablesReplaced);
+
+// fs.writeFileSync("build/less/varia'bles.less", variablesReplaced, "utf8");
