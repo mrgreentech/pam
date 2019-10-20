ef927aa29ad16f75052caef83fe501d777fe5c76
Olle Harju <11613018+ollhar@users.noreply.github.com>
2019-10-20 22:58:05 +0200

    feat: #97 add modifier-helper

---
+++ src/js/modifier-helper.js
@@ -0,0 +1,35 @@
+/* exported modifierHelper */
+
+/**
+ * modifierHelper module replaces encoded colon in modifier examples to bypass pseudo class replacement.
+ * @module modifierHelper
+ * @param  {Object} window
+ * @return {Object}
+ */
+let modifierHelper = (window => {
+    "use strict";
+
+    const modifierWrappers = window.document.querySelectorAll(".kss-modifier__wrapper");
+    const match = "%3A";
+
+    /**
+     * Public API
+     * @type {Object}
+     */
+    const pub = {
+        replaceColon: replaceColon
+    };
+
+    return pub;
+
+    function replaceColon() {
+        [...modifierWrappers].forEach(modifier => {
+            let elementContent = modifier.innerHTML;
+            let hasMatch = elementContent.includes(match);
+
+            if (hasMatch) {
+                modifier.innerHTML = decodeURIComponent(elementContent);
+            }
+        });
+    }
+})(window);
