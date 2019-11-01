6a318c377f558f9ecb66ad7c8abd3057ada4835e
Olle Harju <olle.harju@mrggroup.com>
2019-11-01 09:02:34 +0100

    feat(dist): #105 update dist for beta build
    * 1.0.0-beta.4

--- dist/styleguide/kss-assets/js/styleguide.js
+++ dist/styleguide/kss-assets/js/styleguide.js
@@ -4,43 +4,44 @@
  * Set up custom stuff for the styleguide, such as skinSwitcher and splash animation.
  * @param  {Object} window
  */
-(function (window) {
-  "use strict";
-  /**
-   * skinSwitcher
-   * @type {Object}
-   */
+(function(window) {
+    "use strict";
+    /**
+     * skinSwitcher
+     * @type {Object}
+     */
 
-  var skinSwitcher = window.skinSwitcher;
-  /**
-   * animator
-   * @type {Object}
-   */
+    var skinSwitcher = window.skinSwitcher;
+    /**
+     * animator
+     * @type {Object}
+     */
 
-  var animator = window.animator;
-  /**
-   * Add actions for DOMContentLoaded and load event.
-   */
+    var animator = window.animator;
+    var modifierHelper = window.modifierHelper;
+    /**
+     * Add actions for DOMContentLoaded and load event.
+     */
 
-  document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-  window.onload = init;
-  /**
-   * Enable skinSwitcher on DOMContentLoaded event.
-   * @param  {DOMContentLoadedEvent} evt
-   * @return {undefined}
-   */
+    document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+    window.onload = init;
+    /**
+     * Enable skinSwitcher on DOMContentLoaded event.
+     * @param  {DOMContentLoadedEvent} evt
+     * @return {undefined}
+     */
 
-  function onDOMContentLoaded(evt) {
-    skinSwitcher.enable("main aside header");
-  }
-  /**
-   * Set skinSwitcher select listener and start splash animation.
-   * @return {undefined}
-   */
+    function onDOMContentLoaded(evt) {
+        skinSwitcher.enable("main aside header");
+        modifierHelper.replaceColon();
+    }
+    /**
+     * Set skinSwitcher select listener and start splash animation.
+     * @return {undefined}
+     */
 
-
-  function init() {
-    skinSwitcher.listenOnSelectChange();
-    animator.startAnimation("[sg-hero-img]");
-  }
+    function init() {
+        skinSwitcher.listenOnSelectChange();
+        animator.startAnimation("[sg-hero-img]");
+    }
 })(window);
