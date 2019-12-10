843fd8c0585d5b49622c1e786ba41631fae904b9
BigBoss <11613018+ollhar@users.noreply.github.com>
2019-12-10 16:04:51 +0100

    feat: #122 add display trait
    * add display trait
    * update changelog
    * update dist with latest build
    * update version in package.json to v1.0.0-beta.5

--- dist/styleguide/kss-assets/js/modifier-helper.js
+++ dist/styleguide/kss-assets/js/modifier-helper.js
@@ -1,26 +1,12 @@
 "use strict";
 
-function _toConsumableArray(arr) {
-    return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread();
-}
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
-function _nonIterableSpread() {
-    throw new TypeError("Invalid attempt to spread non-iterable instance");
-}
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
 
-function _iterableToArray(iter) {
-    if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]")
-        return Array.from(iter);
-}
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
 
-function _arrayWithoutHoles(arr) {
-    if (Array.isArray(arr)) {
-        for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) {
-            arr2[i] = arr[i];
-        }
-        return arr2;
-    }
-}
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 /* exported modifierHelper */
 
@@ -30,29 +16,29 @@ function _arrayWithoutHoles(arr) {
  * @param  {Object} window
  * @return {Object}
  */
-var modifierHelper = (function(window) {
-    "use strict";
+var modifierHelper = function (window) {
+  "use strict";
 
-    var modifierWrappers = window.document.querySelectorAll(".kss-modifier__wrapper");
-    var match = "%3A";
-    /**
-     * Public API
-     * @type {Object}
-     */
+  var modifierWrappers = window.document.querySelectorAll(".kss-modifier__wrapper");
+  var match = "%3A";
+  /**
+   * Public API
+   * @type {Object}
+   */
 
-    var pub = {
-        replaceColon: replaceColon
-    };
-    return pub;
+  var pub = {
+    replaceColon: replaceColon
+  };
+  return pub;
 
-    function replaceColon() {
-        _toConsumableArray(modifierWrappers).forEach(function(modifier) {
-            var elementContent = modifier.innerHTML;
-            var hasMatch = elementContent.includes(match);
+  function replaceColon() {
+    _toConsumableArray(modifierWrappers).forEach(function (modifier) {
+      var elementContent = modifier.innerHTML;
+      var hasMatch = elementContent.includes(match);
 
-            if (hasMatch) {
-                modifier.innerHTML = elementContent.replace(/%3A/gi, ":");
-            }
-        });
-    }
-})(window);
+      if (hasMatch) {
+        modifier.innerHTML = elementContent.replace(/%3A/gi, ":");
+      }
+    });
+  }
+}(window);
