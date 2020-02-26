38f2ead3782e79d3ca1cb7db4e2ef7f4afb8310b
Olle Harju <11613018+ollhar@users.noreply.github.com>
2020-02-26 23:10:31 +0100

    feat: #124 update sg drawer animations

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -9,7 +9,9 @@
 let drawer = (window => {
     "use strict";
 
-    const nodeEl = window.document.querySelector("#drawer");
+    const { document } = window;
+    const nodeEl = document.querySelector("[sg-drawer]");
+    const nodeElOverlay = document.querySelector("[sg-drawer-overlay]");
 
     /**
      * Public API
@@ -22,13 +24,31 @@ let drawer = (window => {
     return pub;
 
     function toggle() {
-        if (open()) {
-            console.warn(nodeEl);
-            nodeEl.removeAttribute("open");
-        } else {
-            console.warn(nodeEl);
-            console.warn("set Attr");
+        const nodeElButton = document.querySelector("[sg-drawer-button]");
+        const isButtonHidden = getComputedStyle(nodeElButton).display === "none";
+
+        if (isButtonHidden) {
+            return;
+        }
+
+        return open() ? disable() : enable();
+
+        function enable() {
             nodeEl.setAttribute("open", "");
+            nodeEl.setAttribute("animated", "slideInLeft");
+            nodeElOverlay.setAttribute("animated", "fadeIn");
+            nodeElOverlay.removeAttribute("pam-hidden");
+        }
+
+        function disable() {
+            nodeEl.setAttribute("animated", "slideOutLeft");
+            nodeElOverlay.setAttribute("animated", "fadeOut");
+            setTimeout(() => {
+                nodeEl.removeAttribute("open");
+                nodeElOverlay.setAttribute("pam-hidden", "");
+                nodeEl.removeAttribute("animated");
+                nodeElOverlay.removeAttribute("animated");
+            }, 250);
         }
     }
 
